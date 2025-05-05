071e7e1c828449ba0c4933e421dbff5bc2f62dbe
Priyanshu Sharma <84620013+PriyanshuCK@users.noreply.github.com>
2025-05-05 23:04:26 +0530

    Add line number and shiftwidth options

--- init.lua
+++ init.lua
@@ -2,6 +2,10 @@ print("Namaste")
 
 require("config.lazy")
 
+vim.opt.shiftwidth = 4
+vim.opt.number = true
+vim.opt.relativenumber = true
+
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
