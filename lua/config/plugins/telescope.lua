2a01b83df7e13c2bd32ecb6c12858488332a9f42
Priyanshu Sharma <84620013+PriyanshuCK@users.noreply.github.com>
2025-05-06 20:23:14 +0530

    Update telescope config

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -3,9 +3,25 @@ return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
     -- or                              , branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim', { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' } },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+    },
     config = function()
-      print("telescope")
+      require('telescope').setup {
+        pickers = {
+          find_files = {
+            theme = "ivy"
+          }
+        }
+      }
+      vim.keymap.set("n", "<space>fh", require('telescope.builtin').help_tags)
+      vim.keymap.set("n", "<space>fd", require('telescope.builtin').find_files)
+      vim.keymap.set("n", "<space>en", function()
+        require('telescope.builtin').find_files {
+          cwd = vim.fn.stdpath("config")
+        }
+      end)
     end
   }
 }
