558a8eb553c06ff39fec7fd4830a45fd412ad642
Priyanshu Sharma <84620013+PriyanshuCK@users.noreply.github.com>
2025-05-08 14:34:22 +0530

    Configure gwr run ssl cmd keymap

--- init.lua
+++ init.lua
@@ -20,34 +20,33 @@ vim.keymap.set("n", "-", "<cmd>Oil<CR>")
 -- Highlight when yanking text
 -- See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+    desc = 'Highlight when yanking text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
 vim.api.nvim_create_autocmd('TermOpen', {
-  group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
-  callback = function()
-    vim.opt.number = false
-    vim.opt.relativenumber = false
-  end,
+    group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+    callback = function()
+        vim.opt.number = false
+        vim.opt.relativenumber = false
+    end,
 })
 
 local job_id = 0
 vim.keymap.set("n", "<space>st", function()
-  vim.cmd.vnew()
-  vim.cmd.term()
-  vim.cmd.wincmd("J")
-  vim.api.nvim_win_set_height(0, 5)
+    vim.cmd.vnew()
+    vim.cmd.term()
+    vim.cmd.wincmd("J")
+    vim.api.nvim_win_set_height(0, 5)
 
-  job_id = vim.bo.channel
+    job_id = vim.bo.channel
 end
 )
 
--- Create shortcut to run project script
-vim.keymap.set("n", "<space>exm", function()
-  vim.fn.chansend(job_id, { "echo 'hi' && ls\r\n" })
+vim.keymap.set("n", "<space>gwr", function()
+    vim.fn.chansend(job_id, { "cd src/rendering && npm run start:connected_onpremise_ssl\r\n" })
 end
 )
