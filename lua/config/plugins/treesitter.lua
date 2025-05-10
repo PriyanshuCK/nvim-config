b8a199724145c9bbe157e00de51db915f1a348ea
Priyanshu Sharma <84620013+PriyanshuCK@users.noreply.github.com>
2025-05-10 15:30:42 +0530

    Add js to treesitter config

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     config = function()
       require 'nvim-treesitter.configs'.setup {
         -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-        ensure_installed = { "c", "lua", "typescript", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+        ensure_installed = { "c", "lua", "javascript", "typescript", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
         auto_install = false,
         highlight = {
           enable = true,
