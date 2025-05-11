c972b0bfb611002f9405e28cd6adaef0ff5bee1d
Priyanshu Sharma <84620013+PriyanshuCK@users.noreply.github.com>
2025-05-11 12:11:44 +0530

    Configure telescope acc to kickstart.nvim

--- lua/config/telescope/multigrep.lua
+++ lua/config/telescope/multigrep.lua
@@ -46,7 +46,7 @@ local live_multigrep = function(opts)
 end
 
 M.setup = function()
-  vim.keymap.set("n", "<space>fg", live_multigrep)
+  vim.keymap.set("n", "<leader>sg", live_multigrep)
 end
 
 return M
